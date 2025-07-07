a40608594fcfeb16f9eb8f2eb9d73d82413c8c8d
Davide Mulato <dmulato@d-edge.com>
2025-07-07 14:31:35 +0200

    Implement lenis scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import gsap from "gsap";
+import { ReactLenis } from "lenis/react";
+import { useEffect, useRef } from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -11,16 +13,31 @@ import Contact from "./components/Contact";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
+  const lenisRef = useRef();
+
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
+
   return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Cocktails />
-      <About />
-      <Art />
-      <Menu />
-      <Contact />
-    </main>
+    <>
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
+      <main>
+        <Navbar />
+        <Hero />
+        <Cocktails />
+        <About />
+        <Art />
+        <Menu />
+        <Contact />
+      </main>
+    </>
   );
 };
 
